ef688be56a3f22a5bfa4973cac0de6ca131232c1
Tinashe Kachikoti <tinashekachikoti@gmail.com>
2023-03-21 01:20:50 +0000

    create new to-do items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
-from todo_app.data.session_items import get_items
+from todo_app.data.session_items import get_items, add_item
 
 from todo_app.flask_config import Config
 
@@ -8,8 +8,15 @@ app = Flask(__name__)
 app.config.from_object(Config())
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template(
         'index.html',
         list_of_items=get_items())
+
+@app.route('/add_todo_item', methods=['POST'])
+def add_todo_item():
+
+    add_item(request.form['title'])
+
+    return redirect(url_for('index'))
