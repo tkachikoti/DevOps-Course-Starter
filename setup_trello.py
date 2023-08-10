f9b39efcf54e7746fb2352e286ca6953744f43a8
Tinashe Kachikoti <tinashekachikoti@gmail.com>
2023-08-10 15:03:09 +0100

    update script for flexible string comparison

--- setup_trello.py
+++ setup_trello.py
@@ -1,19 +1,18 @@
 """
-This script updates the Trello-related values in a specified .env file using
-the ID of a given Trello board.
-
-The script requires the following information:
-- Trello API Key
-- Trello API Token
-- Trello Board ID (provided as a command-line argument)
-
-It will fetch the board ID and the IDs of the "Not Started" and "Complete"
-lists from the specified board and update the corresponding values in
-the .env file.
+This module provides a script to update the Trello-related values in a
+specified .env file using the ID of a given Trello board.
+It requires the Trello API key, API token, and Board ID, and updates the IDs
+of the "To Do", "Doing", and "Done" lists on the specified board.
 
 Usage:
-    python script.py <Trello Board ID>
+    poetry run python setup_trello.py
+
+Dependencies:
+    - os
+    - requests
+    - dotenv
 """
+
 
 import os
 import requests
@@ -29,14 +28,27 @@ TRELLO_BOARD_ID = os.getenv("TRELLO_BOARD_ID")
 
 def update_env_file():
     # Get the lists on the board
-    lists_url = f"https://api.trello.com/1/boards/{TRELLO_BOARD_ID}/lists?key={TRELLO_API_KEY}&token={TRELLO_API_TOKEN}"
+    lists_url = (
+        f"https://api.trello.com/1/boards/{TRELLO_BOARD_ID}/lists"
+        f"?key={TRELLO_API_KEY}&token={TRELLO_API_TOKEN}"
+    )
     response = requests.get(lists_url)
     lists = response.json()
 
+    # Function to check list name ignoring case and whitespace
+    def check_list_name(list_, name):
+        return list_['name'].strip().lower() == name.lower()
+
     # Assuming the lists have specific names for "To Do", "Doing" and "Done"
-    trello_todo_list_id = next(list_['id'] for list_ in lists if list_['name'] == 'To Do')
-    trello_doing_list_id = next(list_['id'] for list_ in lists if list_['name'] == 'Doing')
-    trello_done_list_id = next(list_['id'] for list_ in lists if list_['name'] == 'Done')
+    trello_todo_list_id = next(
+        list_['id'] for list_ in lists if check_list_name(list_, 'To Do')
+    )
+    trello_doing_list_id = next(
+        list_['id'] for list_ in lists if check_list_name(list_, 'Doing')
+    )
+    trello_done_list_id = next(
+        list_['id'] for list_ in lists if check_list_name(list_, 'Done')
+    )
 
     # Path to the .env file
     env_file_path = '.env'
@@ -64,5 +76,4 @@ def update_env_file():
 
 
 if __name__ == "__main__":
-
     update_env_file()
